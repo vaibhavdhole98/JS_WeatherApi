431bb9a22ef76508ab5e8d3a075fc08072b66c09
vaibhavdhole98 <88630338+vaibhavdhole98@users.noreply.github.com>
2021-12-27 21:46:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ function showError(error) {
 // API function
 
 function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
     fetch(api).then(function (response) {
         let data = response.json();
         return data;
